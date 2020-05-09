834913457206a70d00c3ccbd509c499968428f13
WilliamFowlerGSU <wf01065@georgiasouthern.edu>
2020-05-09 16:13:48 -0400

    adds firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React, { Component } from 'react'
-import { Route, withRouter } from 'react-router-dom';
-import Home from './components/Home/Home';
-import NavBar from './components/NavBar/NavBar';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
 //import { logDOM } from '@testing-library/react';
 
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+//firebase.analytics();
+
 class App extends Component {
-
   render() {
-    return (
-      <div className="App" >
-        <NavBar />
-        <Route exact path='/' render={(props) => <Home {...props}
 
-        />}
-        />
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+    
+    return (
+      <div className="App">
+        <NavBar />
+        <header className="App-header">
+      {
+        user 
+          ? <p>Hello, {user.displayName}</p>
+          : <p>Please sign in.</p>
+      }
+      {
+        user
+          ? <button onClick={signOut}>Sign out</button>
+          : <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
+    </header>
+        <Route exact path="/" render={(props) => <Home {...props} />} />
       </div>
     );
   }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
