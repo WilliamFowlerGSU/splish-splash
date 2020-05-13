4df17cd620493a67c6f87273f7b0b9c68ffc8dab
kxg6ili <kartheek_gowdra_parameswarappa@homedepot.com>
2020-05-13 12:38:39 -0400

    adds footer

--- src/App.js
+++ src/App.js
@@ -1,56 +1,24 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
-import withFirebaseAuth from "react-with-firebase-auth";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "./firebaseConfig";
-//import { logDOM } from '@testing-library/react';
-
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer";
+
+// styles
 import "./App.css";
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const firebaseAppAuth = firebaseApp.auth();
-
-const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
-};
-
-//firebase.analytics();
 
 class App extends Component {
   render() {
-
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
     
     return (
       <div className="App">
         <NavBar />
-        <header className="App-header">
-      {
-        user 
-          ? <p>Hello, {user.displayName}</p>
-          : <p>Please sign in.</p>
-      }
-      {
-        user
-          ? <button onClick={signOut}>Sign out</button>
-          : <button onClick={signInWithGoogle}>Sign in with Google</button>
-      }
-    </header>
         <Route exact path="/" render={(props) => <Home {...props} />} />
+        <Footer />
       </div>
     );
   }
 }
 
-export default withFirebaseAuth({
-  providers,
-  firebaseAppAuth,
-})(App);
+export default App; 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,29 +1,63 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import ToggleSwitch from './ToggleSwitch/ToggleSwitch'
+import React, { Component } from "react";
+//import { NavLink } from 'react-router-dom'
+//import ToggleSwitch from './ToggleSwitch/ToggleSwitch'
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../../firebaseConfig";
+
 import splishLogo from '../../logos/logo.png'
 import labelLogo from '../../logos/label.png'
 import './NavBar.css';
 
-function NavBar() {
-  const [admin, setAdmin] = useState(false);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
 
-  function toggleAdmin() {
-    setAdmin(!admin);
-  }
-  return (
-    <div>
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+class NavBar extends Component {
+  render() {
+    //const [admin, setAdmin] = useState(false);
+
+    // function toggleAdmin() {
+    //   setAdmin(!admin);
+    // }
+
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    return (
       <div>
-      <div className="header" style={{ justifyContent: "space-between" }}>
-        <header className="App-header">
-          <img src={splishLogo} className="Splish-logo" alt="logo" />
-          <img src={labelLogo} className="Label-logo" alt="logo" />
-        </header>
-          <ToggleSwitch value={admin} label="Admin" toggle={toggleAdmin} />
-           </div>
-      </div>
-    </div >
-  )
+        <div>
+          <div className="header" style={{ justifyContent: "space-between" }}>
+            <header className="App-header">
+              <img src={splishLogo} className="Splish-logo" alt="logo" />
+              <img src={labelLogo} className="Label-logo" alt="nameLogo" />
+              {
+                user
+                  ? <p>Hello, {user.displayName}</p>
+                  : <p>Please sign in.</p>
+              }
+              {
+                user
+                  ? <button onClick={signOut}>Sign out</button>
+                  : <button onClick={signInWithGoogle}>Sign in with Google</button>
+              }
+            </header>
+            {/* <ToggleSwitch value={admin} label="Admin" toggle={toggleAdmin} /> */}
+          </div>
+        </div>
+      </div >
+    )
+  }
 }
 
-export default NavBar
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(NavBar);
