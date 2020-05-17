2891c030aac2264b2134dfa903f7c710ebf07be1
William Fowler <william_m_fowler@homedepot.com>
2020-05-17 03:45:26 -0400

    add SvsPriceList.js

---
+++ src/components/SvsPriceList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SvsPriceList = () => (
+  <div>
+
+    <h2>LAUNDRY SERVICES PRICE LIST</h2>
+    <p>Wash, Dry, Fold, & Hang</p>
+    <p>$1.50/LB</p>
+    <p>10lb minimum/gauranteed 24 hours</p>
+    <p>EXPRESS SERVICE </p>
+    <p>$1.75/LB</p>
+    <h3>gauranteed same day</h3>
+
+    <h2>BEDDING</h2>
+    <h3>COMFORTERS</h3>
+
+  </div>
+);
+
+export default SvsPriceList;
+
